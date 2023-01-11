374de99828074e86b3f89b1b27f5769c149d01ce
Khang Nguyen <36774417+DuyKhangTruong@users.noreply.github.com>
2023-01-11 14:40:10 -0800

    typing-animation-bug
    Install typed.js
    Fixed typing animation with typed.js framework

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,56 +1,46 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-import headerImg from "../assets/img/header-img.svg";
-import gif from "../assets/img/astronut-gif.gif";
-export const Banner = () => {
-    const [loopNum, setLoopNum] = useState(0);
-    const [isDeleting, setIsDeleting] = useState(false);
-    const [text,setText] = useState('');
+import Typed from 'typed.js';
 
-    const toRotate = ["Mobile Engineer","Software Engineer","Computer Geek"];
-    const [delta,setDelta] = useState(200-Math.random() * 10);
-    const period = 300;
+export const Banner = () => {
+
+    const el = useRef(null);
+    // Create reference to store the Typed instance itself
+    const typed = useRef(null);
 
     useEffect(() => {
-        let ticker = setInterval( () =>{
-            tick()
-        },delta);
-
-        return  () =>  {clearInterval(ticker)};
-    }, [text])
-    
-    const tick = () => {
-        let i = loopNum % toRotate.length;
-        let fullText = toRotate[i];
-        let updateText = isDeleting ? fullText.substring(0,text.length - 1) : fullText.substring(0,text.length + 1);
-
-        setText(updateText);
-        if (isDeleting) {
-            setDelta(prevDelta => prevDelta / 2)
+        const options = {
+            strings: ["Mobile Engineer","Software Engineer","Computer Geek"],
+            typeSpeed: 100,
+            backSpeed: 100,
+            loop: true,
+            shuffle:true
+        };
+        
+        // elRef refers to the <span> rendered below
+        typed.current = new Typed(el.current, options);
+        
+        return () => {
+        // Make sure to destroy Typed instance during cleanup
+        // to prevent memory leaks
+        typed.current.destroy();
         }
-        if (!isDeleting && updateText === fullText) {
-            setIsDeleting(true);
-            setDelta(period);
-
-        } else if (isDeleting && updateText === '') {
-            setIsDeleting(false);
-            setLoopNum(loopNum + 1);
-            setDelta(300);
-        }
-
-    }
+    }, [])
 
     return (
         <section className="banner" id="home">
             <Container>
                 <Row className="align-items-center">
-                    <Col xs={12} md = {6} xl={7}>
+                    <Col>
                         <span className="tagline">Welcome stranger to my Portfolio</span>
-                        <h1>{`Hello there, I'm `}<span className="wrap">{text}</span></h1>
-                        <p>You have visited my Portfolio which I decided to make one not for a long time  ago to share my experiences and projects with recruiters. Furthermore, this portfolio will be a canvas for me to practice web development skills with different frameworks. Enjoy it - Khang Nguyen</p>
-                        {/* <button  onClick={(e) => {e.preventDefault(); window.location.replace('/#connect');}}>Let's connect<ArrowRightCircle size={30} /></button> */}
-                        <button>Let's connect<ArrowRightCircle size={30} /></button>
+                        {/* <h1>{`Hello there, I'm `}<span className="wrap">{text}</span></h1> */}
+                        <h1>{`Hello there, I'm `}<br/><span className="wrap" ref={el}/></h1>
+                        <Col xs={12} md={6} xl={7}>
+                            <p>You have visited my Portfolio which I decided to make one not for a long time  ago to share my experiences and projects with recruiters. Furthermore, this portfolio will be a canvas for me to practice web development skills with different frameworks. Enjoy it - Khang Nguyen</p>
+                            {/* <button  onClick={(e) => {e.preventDefault(); window.location.replace('/#connect');}}>Let's connect<ArrowRightCircle size={30} /></button> */}
+                            <button>Let's connect<ArrowRightCircle size={30} /></button>
+                        </Col>
                     </Col>
                 </Row>
             </Container>
